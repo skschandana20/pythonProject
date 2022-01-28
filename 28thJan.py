f684aaa1cbf0e9fd1d72cd9301b6f9cac14a701a
Chandana S <chandana.sc1996@gmail.com>
2022-01-28 16:10:16 +0530

    just temp

---
+++ 28thJan.py
@@ -0,0 +1,28 @@
+# string = "Hello World"
+# d = {}
+# for char in set(string):
+#     d[char]=string.count(char)
+# print(d)
+
+# without using count inbuilt method
+# from collections import defaultdict
+# string = "Hello World"
+# d = {}
+# for char in string:
+#     if char not in d:
+#         d[char] = 1
+#     else:
+#         d[char] += 1
+# print(d)
+
+# s = "Python is a language, Python programming is easy"
+# d = {}
+# l = s.split(" ")
+# for char in l:
+#     if char[0] in "aeiouAEIOU":
+#         d[char] = len(char)
+# print(d)
+
+names = ["apple", "google", "gmail", "yahoo", "gmail", "apple", "gmail", "google"]
+d =
+
